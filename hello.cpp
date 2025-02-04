a0e0b09a399bb71704316b2a5703942a77262591
Miha <stele1000@gmail.com>
2025-02-04 20:37:15 +0100

    Offset change

--- hello.cpp
+++ hello.cpp
@@ -5,13 +5,23 @@ int main()
 {
     InitWindow(800, 450, "Hello, Raylib!");
 
+    int offsetX, offsetY = 0;
+    SetTargetFPS(50);
     while (!WindowShouldClose())
     {
+        if (IsKeyDown(KEY_RIGHT))
+            offsetX++;
+        if (IsKeyDown(KEY_LEFT))
+            offsetX--;
+        if (IsKeyDown(KEY_UP))
+            offsetY--;
+        if (IsKeyDown(KEY_DOWN))
+            offsetY++;
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawText("Hello, World!", 100, 100, 20, RED);
         DrawFPS(10, 10);
-        DrawCircle(GetMouseX(), GetMouseY(), 10, GREEN);
+        DrawCircle(GetMouseX() + offsetX, GetMouseY() + offsetY, 10, GREEN);
         EndDrawing();
     }
 }
